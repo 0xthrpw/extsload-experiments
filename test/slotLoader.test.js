9a42287de0125cbe2eb3590eef01213dd9970bc5
0xthrpw <0xthrpw@gmail.com>
2023-09-08 23:41:43 -0400

    formatting

--- test/slotLoader.test.js
+++ test/slotLoader.test.js
@@ -22,8 +22,8 @@ describe("/ETXSLG/ External Slot Loading General", () => {
 		it("Setup", async () => {})
 
 		it("Set some data", async () => {
-				console.log(`	 Operating from:`, chalk.yellow(admin.address));
-				console.log(`	 `);
+			console.log(`	 Operating from:`, chalk.yellow(admin.address));
+			console.log(`	 `);
 
 			await slotLoader.setStructData(
 				{
@@ -31,10 +31,10 @@ describe("/ETXSLG/ External Slot Loading General", () => {
 					number: 888
 				}
 			);
-				console.log(
-					'	 ',
-					`set first struct as { sender:${admin.address}, number:888 }` 
-				);
+			console.log(
+				'	 ',
+				`set first struct as { sender:${admin.address}, number:888 }` 
+			);
 
 
 			await slotLoader.setStructData(
@@ -43,29 +43,29 @@ describe("/ETXSLG/ External Slot Loading General", () => {
 					number: 777
 				}
 			);
-				console.log(
-					'	 ',
-					`set second struct as { sender:${alice.address}, number:777 }` 
-				);
+			console.log(
+				'	 ',
+				`set second struct as { sender:${alice.address}, number:777 }` 
+			);
 
 			await slotLoader.setMappingData(
 				1,
 				666
 			);
-				console.log(
-					'	 ',
-					`set mapping index 1 as 666` 
-				);
+			console.log(
+				'	 ',
+				`set mapping index 1 as 666` 
+			);
 
 			await slotLoader.setMappingData(
 				22,
 				143
 			);
-				console.log(
-					'	 ',
-					`set mapping index 22 as 143` 
-				);
-				console.log('	 ');
+			console.log(
+				'	 ',
+				`set mapping index 22 as 143` 
+			);
+			console.log('	 ');
 		})
 
 		it("Read some Data", async () => {
@@ -77,15 +77,15 @@ describe("/ETXSLG/ External Slot Loading General", () => {
 			let zeroHex = ethers.utils.hexZeroPad(0, 32);
 			let firstSlot = await slotLoader.extsload(zeroHex);
 			
-				console.log(`	 _version:`);
-				console.log(
-					'	 ', 
-					'slot:', 
-					chalk.yellow(zeroHex), 
-					'value:', 
-					chalk.yellow(firstSlot)
-				);
-				console.log(`	 `);
+			console.log(`	 _version:`);
+			console.log(
+				'	 ', 
+				'slot:', 
+				chalk.yellow(zeroHex), 
+				'value:', 
+				chalk.yellow(firstSlot)
+			);
+			console.log(`	 `);
 
 			/* 
 				Now we'll read the _privateStructs array.  Because this variable 
@@ -99,30 +99,30 @@ describe("/ETXSLG/ External Slot Loading General", () => {
 			// get the data at slot index 1
 			let arrayLength = await slotLoader.extsload(paddedIndex);
 
-				console.log(`	 _privateStructs length:`);
-				console.log(
-					'	 ', 
-					'slot:', 
-					chalk.yellow(paddedIndex), 
-					'value:', 
-					chalk.yellow(arrayLength)
-				);
-				console.log(`	 `);
+			console.log(`	 _privateStructs length:`);
+			console.log(
+				'	 ', 
+				'slot:', 
+				chalk.yellow(paddedIndex), 
+				'value:', 
+				chalk.yellow(arrayLength)
+			);
+			console.log(`	 `);
 
 			// get the first element in the array at slot keccak(0x000...0001)
 			let hashedPaddedIndex = ethers.utils.keccak256(paddedIndex)
 
 			let firstPrivateStruct = await slotLoader.extsload(hashedPaddedIndex);
 			
-				console.log(`	 _privateStructs first element:`);
-				console.log(
-					'	 ', 
-					'slot:', 
-					chalk.yellow(hashedPaddedIndex), 
-					'value:', 
-					chalk.yellow(firstPrivateStruct)
-				);
-				console.log(`	 `);
+			console.log(`	 _privateStructs first element:`);
+			console.log(
+				'	 ', 
+				'slot:', 
+				chalk.yellow(hashedPaddedIndex), 
+				'value:', 
+				chalk.yellow(firstPrivateStruct)
+			);
+			console.log(`	 `);
 
 			// or loop the length of the array and increment across all slots
 			let numberOfSlotsPerStruct = 2;  //address, uint256
@@ -134,7 +134,7 @@ describe("/ETXSLG/ External Slot Loading General", () => {
 
 			let batchedSlots = await slotLoader.extsloadMulti(slots);
 
-				console.log(`	 _privateStructs all elements:`);
+			console.log(`	 _privateStructs all elements:`);
 			
 			for(let e = 0; e < batchedSlots.length; e++){
 				console.log(
@@ -145,31 +145,36 @@ describe("/ETXSLG/ External Slot Loading General", () => {
 					chalk.yellow(batchedSlots[e])
 				);
 			}
-				console.log(`	 `);
+			console.log(`	 `);
 
 			/* 
 				To read mapping data we have to concatenate the index of the 
 				mapping and the slot number.  Because this mapping is keyed on a
-				uint256, our index will be a zero padded number. In this case,
-				the fourth slot.
+				uint256, our index will be a zero padded number. In this case, 
+				index 22 of slot 2.
 			*/
 			let mappingIndex = 22;
 			let slot = 2;
+			
 			let paddedMappingIndex = ethers.utils.hexZeroPad(mappingIndex, 32);
+			
 			let paddedSlot = ethers.utils.hexZeroPad(slot, 32);
+			
 			let concatenated = ethers.utils.concat([paddedMappingIndex, paddedSlot]);
+			
 			let hash = ethers.utils.keccak256(concatenated);
+			
 			let mappingSlot = await slotLoader.extsload(hash);
 
-				console.log(`	 _privateMapping first element:`);
-				console.log(
-					'	 ', 
-					'slot:', 
-					chalk.yellow(hash), 
-					'value:', 
-					chalk.yellow(mappingSlot)
-				);
-				console.log(`	 `);
+			console.log(`	 _privateMapping first element:`);
+			console.log(
+				'	 ', 
+				'slot:', 
+				chalk.yellow(hash), 
+				'value:', 
+				chalk.yellow(mappingSlot)
+			);
+			console.log(`	 `);
 
 		})
 
